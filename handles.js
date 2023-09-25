e135cfdd003d242da8e5bf16fba48a6275401482
MathysBgh <mathys.bagnahgamon@edu.ece.fr>
2023-09-25 16:07:44 +0200

    2.2

--- handles.js
+++ handles.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const url = require('url');
 const qs = require('querystring');
+const db = require('./db'); // Importez la base de données depuis le fichier db.js
+
 
 // Route principale
 router.get('/', (req, res) => {
@@ -21,5 +23,31 @@ router.get('/hello', (req, res) => {
     res.send('Hello anonymous');
   }
 });
+
+// Route pour obtenir tous les articles
+router.get('/articles', (req, res) => {
+  res.json(db.articles);
+});
+
+
+// Route pour ajouter un nouvel article
+router.post('/articles', (req, res) => {
+  const newArticle = req.body; // Supposons que le corps de la requête contient les détails du nouvel article
+  db.articles.push(newArticle);
+  res.status(201).json(newArticle); // Répond avec le nouvel article ajouté
+});
+
+// Route pour obtenir un article par ID
+router.get('/articles/:articleId', (req, res) => {
+  const articleId = req.params.articleId;
+  const article = db.articles.find((article) => article.id === articleId);
+
+  if (!article) {
+    return res.status(404).json({ error: 'Article not found' });
+  }
+
+  res.json(article);
+});
+
 
 module.exports = router;
