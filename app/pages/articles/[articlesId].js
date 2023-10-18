75a7f343f477a7023f8e59742181df8b358e22ec
MathysBgh <mathysbgh@gmail.com>
2023-10-18 19:51:39 +0200

    feat lab6 fini

--- app/pages/articles/[articlesId].js
+++ app/pages/articles/[articlesId].js
@@ -1,50 +1,47 @@
-import { useRouter } from 'next/router';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Nav from '../../components/navbar';
-import { useEffect, useState } from 'react';
 
-function ArticlePage() {
-    const router = useRouter();
-    const [articleId, setArticleId] = useState(null);
+function ArticlePage({ data }) {
+    return (
+        <div className="container mx-auto px-4">
+            <Header />
+            <Nav />
+            <main className="mt-10">
+                <h1 className="text-3xl font-bold mb-4">{data.title}</h1>
+                <p className="text-gray-700">{data.content}</p>
+            </main>
+            <Footer />
+        </div>
+    );
+}
 
-    useEffect(() => {
-        const { articlesId } = router.query;
-        if (articlesId) {
-            setArticleId(articlesId);
-        }
-    }, [router.query]);
+export async function getStaticPaths() {
+    const paths = [
+        { params: { articlesId: '1' } },
+        { params: { articlesId: '2' } },
+        // ... autres IDs d'articles
+    ];
 
-    // Si articleId n'est pas encore disponible, retournez un rendu par défaut
-    if (!articleId) {
-        return (
-            <div className="container mx-auto px-4">
-                <Header />
-                <Nav />
-                <main className="mt-10">
-                    <h1 className="text-2xl font-bold">Chargement...</h1>
-                </main>
-                <Footer />
-            </div>
-        );
-    }
+    return {
+        paths,
+        fallback: 'blocking' // Si un nouvel ID est demandé qui n'est pas dans la liste, Next.js le générera à la demande avec le mode 'blocking'.
+    };
+}
+
+export async function getStaticProps(context) {
+    const articleId = context.params.articlesId;
 
     const dummyData = {
         title: `Titre de l'article ${articleId}`,
         content: `Contenu de l'article ${articleId}. Ceci est un exemple de données factices pour l'article ${articleId}.`
     };
 
-    return (
-        <div className="container mx-auto px-4">
-            <Header />
-            <Nav />
-            <main className="mt-10">
-                <h1 className="text-3xl font-bold mb-4">{dummyData.title}</h1>
-                <p className="text-gray-700">{dummyData.content}</p>
-            </main>
-            <Footer />
-        </div>
-    );
+    return {
+        props: {
+            data: dummyData
+        }
+    };
 }
 
 export default ArticlePage;
