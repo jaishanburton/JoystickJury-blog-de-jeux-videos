86ea80c5f7ea0bc8c8c5fef85e1b1f0b92a013c9
slkjayy93 <74787049+slkjayy93@users.noreply.github.com>
2023-11-12 20:04:48 +0100

    feat: Récuperation des données depuis Supabase

--- app/components/LoggedIn.js
+++ app/components/LoggedIn.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { useUser } from './UserContext';
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function LoggedIn() {
-    const { user, logout } = useUser();
+    const { user } = useUser();
+    const supabaseClient = useSupabaseClient();
+
+    const logout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        if (error) {
+            console.error('Erreur de déconnexion:', error);
+        }
+    };
 
     return (
         <div>
             <div>
-                <span role="img" aria-label="icône de compte">👤</span> {user.username}
+                <span role="img" aria-label="icône de compte">👤</span> {user?.email}
             </div>
             <button onClick={logout}>Se déconnecter</button>
         </div>
