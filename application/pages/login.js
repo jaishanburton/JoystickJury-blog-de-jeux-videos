9fc7a99f5b6dcb246e0a255f0b50c4069d42f00c
MathysBgh <mathysbgh@gmail.com>
2023-11-21 02:14:46 +0100

    feat: lab 9 partie 1

---
+++ application/pages/login.js
@@ -0,0 +1,36 @@
+// Fichier: pages/login.js
+
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared'; // Assurez-vous d'importer ThemeSupa
+import { supabase } from '../supabaseClient'; // Assurez-vous que le chemin est correct
+
+const LoginPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') {
+        router.push('/profile');
+      }
+    });
+
+    // Si la méthode unsubscribe n'existe pas, ne faites rien lors du nettoyage
+    return () => {
+      if (authListener && typeof authListener.unsubscribe === 'function') {
+        authListener.unsubscribe();
+      }
+    };
+  }, [router]);
+
+  return (
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }} // Appliquez le thème ThemeSupa ici
+      providers={['google', 'facebook', 'twitter']}
+    />
+  );
+};
+
+export default LoginPage;
---
+++ application/pages/profile.js
@@ -0,0 +1,44 @@
+// Fichier: pages/profile.js
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { supabase } from '../supabaseClient'; // Assurez-vous que le chemin est correct
+
+const ProfilePage = () => {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        setUsername(session.user.user_metadata.full_name);
+        setEmail(session.user.email);
+        setLoading(false);
+      } else {
+        router.push('/login');
+      }
+    });
+  }, [router]);
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.push('/login');
+  };
+
+  if (loading) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Profil</h1>
+      <p>Nom d'utilisateur: {username}</p>
+      <p>Email: {email}</p>
+      <button onClick={handleLogout}>Se déconnecter</button>
+    </div>
+  );
+};
+
+export default ProfilePage;
