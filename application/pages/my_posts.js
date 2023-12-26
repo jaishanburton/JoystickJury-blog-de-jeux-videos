09c72b2d0a5438bae7a7e6629c3cadb52b8a535a
slkjayy93 <74787049+slkjayy93@users.noreply.github.com>
2023-12-26 03:57:16 +0100

    feat: possibilite de supprimer son post

--- application/pages/my_posts.js
+++ application/pages/my_posts.js
@@ -6,11 +6,14 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { createClient } from '@supabase/supabase-js';
+import { TrashIcon } from '@heroicons/react/outline'; 
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 // Initialisez le client Supabase ici, en dehors du composant
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 // Paramètres pour le carrousel
 const settings = {
@@ -25,17 +28,37 @@ const settings = {
 
 const MyPosts = () => {
   const [myPosts, setMyPosts] = useState([]);
+
+  const supabaseClient = useSupabaseClient();
+  const [userEmail, setUserEmail] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
   const router = useRouter();
+
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        setUserEmail(session.user.email);
+        setLoading(false);
+      } else {
+        setLoading(false);
+        setUserEmail(null);
+      }
+    });
+  }, [supabaseClient]);
+  
 
   useEffect(() => {
     async function fetchMyPosts() {
       const { data: { session } } = await supabase.auth.getSession();
       
       if (session) {
+        console.log('Fetching posts for email:', session.user.email); // Log pour déboguer
         let { data: fetchedPosts, error } = await supabase
           .from('posts')
           .select('*')
-          .eq('email', session.user.email); // Filtre par email de l'utilisateur
+          .eq('email', session.user.email);
 
         if (error) {
           console.log('Error fetching posts', error);
@@ -48,31 +71,74 @@ const MyPosts = () => {
     fetchMyPosts();
   }, []);
 
+
+  // Gérer le clic sur le post pour naviguer vers la page de détail
   const handlePostClick = (post) => {
     router.push({
       pathname: `/posts/${post.id}`,
     });
   };
 
+ // Gérer la suppression du post
+ const handleDelete = async (postId) => {
+    if (!userEmail) {
+      alert('Vous devez être connecté pour supprimer des posts.');
+      return;
+    }
+  
+    if (window.confirm('Êtes-vous sûr de vouloir supprimer ce post ?')) {
+      try {
+        // Démarrez une transaction pour supprimer les commentaires puis le post
+        await supabaseClient
+          .from('comments')
+          .delete()
+          .eq('post_id', postId);
+  
+        const { error } = await supabaseClient
+          .from('posts')
+          .delete()
+          .match({ id: postId, email: userEmail });
+  
+        if (error) {
+          throw error;
+        }
+  
+        // Mettre à jour l'état local après la suppression réussie
+        setMyPosts(currentPosts => currentPosts.filter((post) => post.id !== postId));
+        alert('Post et commentaires associés supprimés avec succès.');
+  
+      } catch (error) {
+        alert(`Erreur lors de la suppression : ${error.message}`);
+        console.log('Error in delete transaction:', error);
+      }
+    }
+  };
+  
+  
   return (
     <>
       <Header />
       <main className="min-h-screen bg-gray-100">
-        {/* Posts Section */}
+        {/* Section des posts */}
         <section className="container mx-auto py-20">
           <h2 className="text-4xl text-center font-bold mb-12">Mes Posts</h2>
           <Slider {...settings}>
             {myPosts.map((post, index) => (
-              <div key={index} className="p-2 post-card">
+              <div key={index} className="p-2 post-card relative">
                 <div
                   className="bg-white rounded-lg overflow-hidden shadow-lg h-full flex flex-col transform transition duration-300 hover:scale-105 cursor-pointer"
                   onClick={() => handlePostClick(post)}
                 >
-                  <img src={post.nom_image} alt={post.title} className="w-full object-contain" style={{ height: '250px' }} />
+                  <img src={post.nom_image} alt={post.nom_du_jeu} className="w-full object-contain" style={{ height: '250px' }} />
                   <div className="p-6 flex flex-col justify-between flex-grow">
                     <h3 className="font-bold text-xl mb-2">{post.nom_du_jeu}</h3>
                     <p className="text-gray-700 text-base">{post.contenu_du_jeu}</p>
                   </div>
+                  {/* Icône de la poubelle pour la suppression */}
+                  <TrashIcon className="h-6 w-6 absolute top-2 right-2 text-red-600 cursor-pointer" onClick={(e) => {
+                    e.stopPropagation(); // Empêcher l'événement de clic de la carte
+                    handleDelete(post.id);
+                  }} />
                 </div>
               </div>
             ))}
