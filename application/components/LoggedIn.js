d17009cca097a0cee828232225e7fbe8fb91f2eb
Mathys <mathysbgh@gmail.com>
2023-12-26 15:08:07 +0100

    fix: email affiché après connexion

--- application/components/LoggedIn.js
+++ application/components/LoggedIn.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { createClient } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 
 function LoggedIn() {
     const { user } = useUser();
     const supabaseClient = useSupabaseClient();
+    const [userEmail, setUserEmail] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [username, setUsername] = useState(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          if (session) {
+            setUserEmail(session.user.email);
+            setLoading(false);
+          } else {
+            setLoading(false);
+            setUserEmail(null);
+          }
+        });
+      }, [supabaseClient]);
+      
+
 
     const logout = async () => {
         const { error } = await supabaseClient.auth.signOut();
@@ -15,7 +43,7 @@ function LoggedIn() {
     return (
         <div>
             <div>
-                <span role="img" aria-label="icône de compte">👤</span> {user?.email}
+                <span role="img" aria-label="icône de compte">👤</span> {userEmail}
             </div>
             <button onClick={logout}>Se déconnecter</button>
         </div>
