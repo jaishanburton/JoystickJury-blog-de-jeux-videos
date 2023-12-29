eb080f4929652e2697c8ea3f25c4ece8cbc4a9ee
Mathys <mathysbgh@gmail.com>
2023-12-29 15:13:30 +0100

    fix: header gravatar

--- application/components/LoggedIn.js
+++ application/components/LoggedIn.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { createClient } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
+import md5 from 'md5';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -18,6 +19,12 @@ function LoggedIn() {
     const [loading, setLoading] = useState(true);
     const [username, setUsername] = useState(null);
     const router = useRouter();
+
+    const getGravatarUrl = (email) => {
+        const hash = md5(email.trim().toLowerCase());
+        return `https://www.gravatar.com/avatar/${hash}?d=identicon&s=200`;
+    };
+
 
     useEffect(() => {
       supabase.auth.getSession().then(({ data: { session } }) => {
@@ -53,8 +60,9 @@ function LoggedIn() {
 
     return (
         <div>
-            <div>
-                <span role="img" aria-label="icône de compte">👤</span> {userEmail}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                {userEmail && <img src={getGravatarUrl(userEmail)} alt="Gravatar" style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '10px' }} />}
+                {userEmail}
             </div>
             <button onClick={logout}>Se déconnecter</button>
         </div>
